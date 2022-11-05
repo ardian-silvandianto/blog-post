2601096bb3d86dad533bf889085f05049fe3be37
asilvandianto <ardian.silvandianto@gmail.com>
2022-11-05 15:21:07 +1300

    Update removePost.test.js
    Add a test check that the post that is about to be deleted exists

--- test/removePost.test.js
+++ test/removePost.test.js
@@ -6,7 +6,17 @@ chai.use(chaiHttp);
 
 
 describe('Delete a single blog post', () => {
-    it('Delete a post', (done) => {
+    it('Check that the post exists', (done) => {
+        let id = 12
+        chai.request(baseURL)
+            .get('/posts/' + id)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                done();
+            });
+    }); it('Delete a post', (done) => {
         let id = 12
         chai.request(baseURL)
             .delete('/posts/' + id)
